b360d30d22194607fb155adad3102fc0627fa13f
eliranazulay <eliran@moveohls.com>
2022-02-03 14:35:10 +0200

    Clear local state

--- infra-modules/network/network.tf
+++ infra-modules/network/network.tf
@@ -2,24 +2,25 @@ provider "aws" {
   region = var.region
 }
 
-module "private_netwotk" {
- source               = "./vpc-private"
- stage                = var.stage  
- name                 = var.name
- availability_zones   = var.availability_zones  
- context              = module.this.context
- region              = var.region
-}
-
-
-
-
-# module "private_public_netwotk" {
-#  source     = "./vpc-private-public"
- 
+# module "private_netwotk" {
+#  source               = "./vpc-private"
+#  stage                = var.stage  
+#  name                 = var.name
 #  availability_zones   = var.availability_zones  
-#  context = module.this.context
+#  context              = module.this.context
+#  region              = var.region
 # }
+
+
+# Most of the application you will need to use this network
+# Use this vpc if you need an internet network
+
+module "private_public_netwotk" {
+ source     = "./vpc-private-public"
+ 
+ availability_zones   = var.availability_zones  
+ context = module.this.context
+}
 
 
 
