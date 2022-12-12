5a22ac185416cb91986ebd3ee04863417da34823
barak <barak@moveohls.com>
2022-12-12 12:02:11 +0200

    no message

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,11 +1,12 @@
 
 resource "random_password" "db_user_password" {
   length = 24
+  special = false
 }
 
 locals {
   database_name     = "${var.context.name}${var.context.stage}"
-  database_user     = "${var.context.name}-${var.context.stage}-user"
+  database_user     = "${var.context.name}_${var.context.stage}_user"
   database_password = random_password.db_user_password.result
 
 }
