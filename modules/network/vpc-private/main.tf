ac0ea763dcab128dcdc661039106ce5510916fef
eliran azulay <eliran@moveohls.com>
2023-05-24 17:35:08 +0300

    fix: private vpc module

--- modules/network/vpc-private/main.tf
+++ modules/network/vpc-private/main.tf
@@ -3,9 +3,7 @@ module "vpc" {
   source  = "cloudposse/vpc/aws"
   version = "2.1.0"
 
-  cidr_block               = "172.16.0.0/16"
-  enable_dns_support       = true
-  enable_dns_hostnames     = true
+  ipv4_primary_cidr_block  = "172.16.0.0/16"
   internet_gateway_enabled = false
   context                  = var.context
 }
@@ -17,8 +15,6 @@ module "private_subnets" {
   availability_zones = var.availability_zones
   vpc_id             = module.vpc.vpc_id
   cidr_block         = module.vpc.vpc_cidr_block
-  enabled            = true
-  context            = var.context
 }
 
 
@@ -26,7 +22,6 @@ module "vpc_endpoints" {
   source = "../vpc-endpoints"
 
   vpc_id                  = module.vpc.vpc_id
-  cidr_block              = module.vpc.vpc_cidr_block
   private_subnet_ids      = module.private_subnets.private_subnet_ids
   private_route_table_ids = module.private_subnets.private_route_table_ids
   default_security_group  = module.default_security_group.id
@@ -45,7 +40,7 @@ module "default_security_group" {
   source = "../security-group"
 
   vpc_id            = module.vpc.vpc_id
-  ipv4_cidr_block   = [module.vpc.vpc_cidr_block]
+  vpc_cidr_block    = module.vpc.vpc_cidr_block
   s3_prefix_list_id = module.vpc_endpoints.aws_vpc_endpoint_s3
   context           = var.context
 }
