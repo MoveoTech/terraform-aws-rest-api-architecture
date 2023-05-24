144a6cad3812413c2b7941e02db9460e2eb151d5
eliran azulay <eliran@moveohls.com>
2023-05-24 21:09:41 +0300

    fix: change names

--- modules/backend/api-gateway/main.tf
+++ modules/backend/api-gateway/main.tf
@@ -12,13 +12,13 @@ module "account_settings" {
   source = "cloudposse/api-gateway/aws//modules/account-settings"
 
   version = "0.3.1"
-  name    = "api-gateway-${var.context.stage}"
+  name    = var.context.name
 
 }
 
 
 resource "aws_api_gateway_rest_api" "main" {
-  name               = "api-gateway-${var.context.stage}"
+  name               =  var.context.name
   binary_media_types = var.binary_media_types
   tags = merge(var.context.tags, { Name = "Api Gateway" }, {
     yor_trace = "921a6956-bab5-4ab5-9fca-496360259651"
