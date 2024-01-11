17a8642ec2f24bac26ad4685f56516818f1617df
bar varfman <>
2024-01-11 15:20:38 +0200

    fix: fix bug for atlas-project

--- modules/database/atlas-project/main.tf
+++ modules/database/atlas-project/main.tf
@@ -1,17 +1,9 @@
 
 resource "mongodbatlas_project" "project" {
-  count = var.team_id ? 1 : 0
   name   = "${var.context.name}-${var.context.stage}"
   org_id = var.atlas_org_id
   teams {
     team_id    = var.team_id
     role_names = var.role_names
   }
-}
-
-
-resource "mongodbatlas_project" "project" {
-  count = var.team_id ? 0 : 1
-  name   = "${var.context.name}-${var.context.stage}"
-  org_id = var.atlas_org_id
 }
